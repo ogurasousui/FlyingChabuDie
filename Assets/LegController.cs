850b6610caf172d6c8f026f5688044674e2b288b
ogurasousui <ogurasousui2@gmail.com>
2018-07-02 23:57:26 +0900

    Using github unity

---
+++ Assets/LegController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegController : MonoBehaviour {
+	public char setKey;
+	Rigidbody2D rigid2D;
+
+	float upForce = 20.0f;
+	float upForceOther = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((this.setKey.Equals('r') && Input.GetKey(KeyCode.RightArrow)) || (this.setKey.Equals('l') && Input.GetKey(KeyCode.LeftArrow))) {
+			this.rigid2D.AddForce(transform.up * this.upForce);
+		}
+        if ((this.setKey.Equals('l') && Input.GetKey(KeyCode.RightArrow)) || (this.setKey.Equals('r') && Input.GetKey(KeyCode.LeftArrow)))
+        {
+            this.rigid2D.AddForce(transform.up * this.upForceOther);
+        }
+	}
+}
---
+++ Assets/TableController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableController : MonoBehaviour {
+	Rigidbody2D rigid2D;
+
+	// Use this for initialization
+	void Start () {
+		this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        	if (Input.GetKey(KeyCode.Space)) {
+			this.rigid2D.velocity = Vector2.zero;
+			transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+		}
+	}
+}
